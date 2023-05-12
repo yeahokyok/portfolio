0a7e26d808167a6885273fb316f944130af64430
yeahokyok <982466+yeahokyok@users.noreply.github.com>
2023-05-12 20:29:51 +0700

    skills no scale so it won't top of nav

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,10 +14,7 @@ import { TbApi, TbBrandGolang } from "react-icons/tb"
 
 const Skills = () => {
     return (
-        <div
-            name="skills"
-            className="h-screen w-full bg-gray-900 text-gray-300"
-        >
+        <div name="skills" className="h-full w-full bg-gray-900 text-gray-300">
             {/* Container */}
             <div className="mx-auto flex h-full w-full max-w-[1000px] flex-col justify-center p-4">
                 <div>
@@ -30,48 +27,48 @@ const Skills = () => {
                 </div>
 
                 <div className="grid w-full grid-cols-2 gap-4 py-8 text-center sm:grid-cols-4">
-                    <div className="duration-500 hover:scale-110">
+                    <div className="duration-500">
                         <SiPython size={50} className="mx-auto" />
                         <p className="my-4">Python</p>
                     </div>
-                    <div className="duration-500 hover:scale-110">
+                    <div className="duration-500">
                         <SiDjango size={50} className="mx-auto" />
                         <p className="my-4">Django</p>
                     </div>
-                    <div className="duration-500 hover:scale-110">
+                    <div className="duration-500">
                         <SiPostgresql size={50} className="mx-auto" />
                         <p className="my-4">Postgresql</p>
                     </div>
-                    <div className="duration-500 hover:scale-110">
+                    <div className="duration-500">
                         <TbApi size={50} className="mx-auto" />
                         <p className="my-4">RESTful API</p>
                     </div>
-                    <div className="duration-500 hover:scale-110">
+                    <div className="duration-500">
                         <SiSolidity size={50} className="mx-auto" />
                         <p className="my-4">Solidity</p>
                     </div>
-                    <div className="duration-500 hover:scale-110">
+                    <div className="duration-500">
                         <SiEthereum size={50} className="mx-auto" />
                         <p className="my-4">Ethereum</p>
                     </div>
 
-                    <div className="duration-500 hover:scale-110">
+                    <div className="duration-500">
                         <SiJavascript size={50} className="mx-auto" />
                         <p className="my-4">Javascript</p>
                     </div>
-                    <div className="duration-500 hover:scale-110">
+                    <div className="duration-500">
                         <SiReact size={50} className="mx-auto" />
                         <p className="my-4">React</p>
                     </div>
-                    <div className="duration-500 hover:scale-110">
+                    <div className="duration-500">
                         <TbBrandGolang size={50} className="mx-auto" />
                         <p className="my-4">Golang</p>
                     </div>
-                    <div className="duration-500 hover:scale-110">
+                    <div className="duration-500">
                         <SiDocker size={50} className="mx-auto" />
                         <p className="my-4">Docker</p>
                     </div>
-                    <div className="duration-500 hover:scale-110">
+                    <div className="duration-500">
                         <SiGit size={50} className="mx-auto" />
                         <p className="my-4">Git</p>
                     </div>
